d1f41edfbd890a7ec871fefc7cfed3c76ab685f8
Contrigra <gaponov60b@gmail.com>
2021-01-20 17:01:12 +0300

    Done branch

--- api/views.py
+++ api/views.py
@@ -1,40 +1,41 @@
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from slugify import slugify
+
 from api.models import Ingredient
 from .forms import RecipeForm
-from slugify import slugify
 
 
 @login_required
 def create_recipe_view(request):
     """
-    View function for a recipe creation page
+    View function for a recipe creation page. Parses tags from checkboxes
+    input, prepares the data for the tag field.
     """
     _choices = {'breakfast': 'breakfast',
-               'lunch': 'lunch',
-               'dinner': 'dinner'}
+                'lunch': 'lunch',
+                'dinner': 'dinner'}
 
     if request.method == 'POST':
-        # transforming received tag checkbox to acceptable input for the form
+        # transforming received tag checkbox to acceptable (
+        # space-delimited string) input for the form and modify POST request
+        # to include acceptable field data.
+
         # Should I create another function for this or this is acceptable
-        # since it's the necessary part of recipe creation? Requires a
-        # space delimited string
+        # since it's the necessary part of the recipe creation? And will be
+        # used only here?
+
         _tag = ''
         for x in _choices:
             if x in request.POST:
                 _tag += ' ' + _choices[x]
 
-        print(_tag)
         post = request.POST.copy()
         post['tag'] = _tag
-
         request.POST = post
-        form = RecipeForm(request.POST or None, files=request.FILES or None)
+        form = RecipeForm(request.POST or None, request.FILES or None)
 
-
-        # TODO сделать загрузку картинок и времени тэги
         if form.is_valid():
             recipe = form.save(commit=False)
             recipe.author = request.user
@@ -45,8 +46,6 @@ def create_recipe_view(request):
 
             return redirect(to='/', username=request.user.username,
                             permanent=True)
-
-
 
     form = RecipeForm()
 
