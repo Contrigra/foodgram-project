41fe5102799bf5b7349b4be54be8548477b9cad6
Contrigra <gaponov60b@gmail.com>
2021-03-09 23:07:17 +0300

    need to refactor types to tags

--- foodgram/templatetags/recipe_tags.py
+++ foodgram/templatetags/recipe_tags.py
@@ -3,8 +3,13 @@ from django import template
 register = template.Library()
 
 @register.filter()
-def add_id(url, tag):
-    url_line = str(tag)
+def add_id(url, type_id):
+    url_line = str(type_id)
     if url is None:
         return url_line
-    return url + '&tags=' + url_line
+    return url + url_line
+
+
+@register.filter()
+def string_view(type_id):
+    return str(type_id)
--- foodgram/utils.py
+++ foodgram/utils.py
@@ -13,14 +13,25 @@ def sum_ingredients(ingredient_list):
                           key=key)]
     return ingredient_list
 
-def get_tags(request):
 
+def get_url_with_types(request):
+    url_type_line = request.GET.get('type')
+    return url_type_line
+
+
+def get_filter_type(request):
     try:
-        tags = request.GET.getlist('tags')
+        given_types = list(request.GET.get('type'))
     except TypeError:
-        tags = None
-    return tags
+        given_types = None
+    return given_types
 
-def get_url_with_tags(request):
-    url_tags_line = request.GET.get('tags')
-    return url_tags_line
+
+def get_types(data):
+    types_list = []
+    for key in data:
+        if key != 'picture' and key != 'picture-clear':
+            if data[key] == 'on':
+                types_list.append(key)
+    return types_list
+
