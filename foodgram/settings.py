099970f5e297c6477f43e17f375c447de29cbc1e
Contrigra <gaponov60b@gmail.com>
2021-03-24 00:19:29 +0300

    trying figure out deploying

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -120,11 +120,11 @@ SITE_ID = os.environ.get('SITE_ID')
 
 
 # TODO проверить, со слешем вконце
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 TAGGIT_CASE_INSENSITIVE = True
 
