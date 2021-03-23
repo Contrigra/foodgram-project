561cd4be342b3c4d4d68cf08a9bbb083841a6ba7
Contrigra <gaponov60b@gmail.com>
2021-03-23 20:29:08 +0300

    trying figure out deploying

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -119,13 +119,9 @@ SITE_ID = os.environ.get('SITE_ID')
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-        ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
