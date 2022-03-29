dadf87b4439bde4a97dded86643b4cb3194d46ad
Contrigra <gaponov60b@gmail.com>
2022-03-29 16:04:46 +0300

    fix(dependencies): returned the old django version.
    New version breaks too many things

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
